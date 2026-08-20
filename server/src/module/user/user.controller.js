e3d717da139c9efff48c0fcf1f1ef55de231a169
abdulrhman-elghnam <abdulrhman.mohamed.atya@gmail.com>
2026-08-21 00:10:17 +0300

    Update user creation route and add middleware structure

--- server/src/module/user/user.controller.js
+++ server/src/module/user/user.controller.js
@@ -2,7 +2,7 @@ import structure from '#/common/structure/index.js'
 import express from 'express';
 const router = express();
 
-router.post("/create" ,(req,res,next)=>{
+router.post("/create" ,()=>{
     structure.sendSuccess()
 })
 export default router;
